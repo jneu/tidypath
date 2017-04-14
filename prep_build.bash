c8dd1ff10556d71145da9676b9cfbf621c04ca05
Joshua Neuheisel <joshua@neuheisel.us>
2017-04-14 11:19:55 -0400

    added comments in the build files

--- prep_build.bash
+++ prep_build.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#
+# Call autoreconf to recreate the build files. When necessary, invoke
+# as `prep_build.bash --force` to force a rebuild of everything.
+#
+
 exec autoreconf --install "$@"
